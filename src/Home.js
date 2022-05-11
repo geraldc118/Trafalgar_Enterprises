887dbb7d8ff0c18b07c84f23c8cdc82e382a5b47
Gerald <geraldcameron118@gmail.com>
2022-05-11 17:02:25 +0100

    changes to home page

--- src/Home.js
+++ src/Home.js
@@ -8,8 +8,8 @@ export default function Home(){
     <Header />
     <main>
         <section className='home_section'>
-        <div className='welcome'>
-            <h2>Welcome</h2>
+        <div className='welcome_container'>
+            <h1 className='welcome_title'>Welcome</h1>
             <p>We specialise in shipping container conversions that provide eco-friendly bespoke units to businesses and individuals.</p>
             <p>Non converted containers are also available in different sizes,with price dependant on the age and size of the container.</p>
             <p>For more information on Trafalgar Enterprises can do for you, please send us an enquiry or give us a call.</p>
