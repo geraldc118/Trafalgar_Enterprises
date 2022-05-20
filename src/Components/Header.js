fca498cc244e36f58098e1fd38af39a9bd12136a
Gerald <geraldcameron118@gmail.com>
2022-05-20 23:02:54 +0100

    added containers for mobile making it responisve

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,9 +6,9 @@ import NavBar from "../Components/Navbar"
 
 function Header() {
   return (<section>
-   <div className="header_container">
    <NavBar />
-      <header>
+   <div className="header_container">
+      <header className=''>
       <div className='container'>
         <div className="head_info_container">
           <h1 className="header_title">Trafalgar Enterprises</h1>
@@ -18,13 +18,14 @@ function Header() {
           <div className='btn_container'>
           <a href="/Containers" className="btn btn-primary btn-lg" role="button">View Containers</a>
           </div>
-        
-          <div className="container-md centered">
-          <i className="header_icon fas fa-phone-alt"></i><a className=" head_info" href="tel:02086621479">Tel:02086621479</a>
-          <i className="header_icon fas fa-envelope"></i><a className=" head_info" href="mailto:info@trafalgerenterprises.com@gmail.com">Email:info@trafalgerenterprises.com@gmail.com</a>
           </div>
         </div>
-        </div>
+        
+          <div className="container">
+            <i className="header_icon fas fa-phone-alt"></i><a className="head_info" href="tel:02086621479">Tel:02086621479</a>
+            <i className="header_icon fas fa-envelope"></i><a className="head_info" href="mailto:info@trafalgerenterprises.com@gmail.com">Email:info@trafalgerenterprises.com@gmail.com</a>
+          </div>
+
       </header>
     </div>
     </section>
